93d213e3e7876ab15af51d862ab49de6ff603f5f
VR-Blackbird <venkataraman1503@gmail.com>
2022-05-04 10:15:09 +0530

    Code clean up

--- Tree.py
+++ Tree.py
@@ -10,41 +10,41 @@ class Node:
     def __repr__(self) -> str:
         return f"Node--{self.parent}"
 
-    def pre_order(self, node):
-        yield node
-        if node.left is not None:
-            yield from self.pre_order(node.left)
-        if node.right is not None:
-            yield from self.pre_order(node.right)
 
-    def in_order(self, node):
-        if node is None:
-            return
-        self.in_order(node.left)
-        print(node)
-        self.in_order(node.right)
+def pre_order(node):
+    print(node)
+    if node.left is not None:
+        pre_order(node.left)
+    if node.right is not None:
+        pre_order(node.right)
 
-    def post_order(self, node):
-        if node is None:
-            return
-        self.post_order(node.left)
-        self.post_order(node.right)
-        print(node)
 
-    def level_order(self):
-        container = deque()
-        container.append(self)
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node)
+    in_order(node.right)
 
-        while container:
-            element = container.popleft()
-            yield element
-            if element.left:
-                container.append(element.left)
-            if element.right:
-                container.append(element.right)
 
-    def get_children(self):
-        print(self.childred)
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node)
+
+
+def level_order(node):
+    container = deque()
+    container.append(node)
+    while container:
+        element = container.popleft()
+        yield element
+        if element.left:
+            container.append(element.left)
+        if element.right:
+            container.append(element.right)
 
 
 child3 = Node(5)
@@ -55,4 +55,13 @@ child1 = Node(1, child3, child4)
 child2 = Node(2, child5, child6)
 root = Node(3, child1, child2)
 
-root.in_order(root)
+print("-" * 38)
+print("InOrder Traversal of binary Tree\n")
+in_order(root)
+print("-" * 38)
+print("Pre-order Traversal of a binary Tree\n")
+pre_order(root)
+print("-" * 38)
+print("Post-order Traversal of a binary Tree\n")
+post_order(root)
+print("-" * 38)
